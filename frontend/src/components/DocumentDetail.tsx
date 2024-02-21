c7ffdb206ff624def567b34c475abb479ca54bab
Liz Tianjiao Liu <tianjiaoliu2012@gmail.com>
2024-02-21 09:21:17 -0800

    Update DocumentDetail.tsx

--- frontend/src/components/DocumentDetail.tsx
+++ frontend/src/components/DocumentDetail.tsx
@@ -14,17 +14,8 @@ const DocumentDetail: React.FC<Document> = (document: Document) => {
   return (
     <>
       <h3 className="text-center mb-3 text-lg font-bold tracking-tight text-gray-900">
-        {document.filename}
+        {document.filename.replace(/\.pdf$/i, '')}
       </h3>
-      <div className="flex flex-col space-y-2">
-        <div className="inline-flex items-center">
-          <DocumentIcon className="w-4 h-4 mr-2" />
-          {document.pages} pages
-        </div>
-        <div className="inline-flex items-center">
-          <CircleStackIcon className="w-4 h-4 mr-2" />
-          {filesize(Number(document.filesize)).toString()}
-        </div>
         <div className="inline-flex items-center">
           <ClockIcon className="w-4 h-4 mr-2" />
           {getDateTime(document.created)}
